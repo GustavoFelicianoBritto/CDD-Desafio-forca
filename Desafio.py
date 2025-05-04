c1eb481a459a3557f8898411f277dea86a621cd2
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-05-04 19:45:24 -0300

    Adição de boneco visual simples para forca, correção de saída

--- Desafio.py
+++ Desafio.py
@@ -1,4 +1,63 @@
+forcaDraw = [
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+========""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+========""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+========""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+========""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+========""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========"""
+]
 
+estagioForca=0
 
 palavra = input("Jogador desafiante, digite a palavra para o jogador 2: ").lower() or "Carro".lower()
 if palavra=="carro":
@@ -16,7 +75,6 @@ for i in range(50):
     print("="*20)
     print("Não suba, não trapaceie")
 
-#print(f"Debug, a palavra é: {activeWord}")
 print(f"\nBem vindo ao jogo da forca!!! \n"
       f"Dica: {categoria}\n")
 
@@ -26,21 +84,15 @@ def displayWord():
 
     return " , ".join(wordGuess[:letterQtd])
 
-
-
-
 while acertos < letterQtd and vidas>0:
-
     notValid = 0
-
     print(f"Letras enviadas: {letterSend}")
-
+    print(forcaDraw[estagioForca])
     dp = displayWord().upper()
     letter = str(input(f"{dp}\n\nDigite uma letra: ").lower())
 
     if letter not in letterSend:
-        #se a letra enviada não estiver no vetor das letras enviadas, irei colocar ela dentro e
-        #darei sequencia no código
+        #se a letra enviada não estiver no vetor das letras enviadas, irei colocar ela dentro e darei sequencia no código
 
         letterSend.append(letter)
 
@@ -48,6 +100,8 @@ while acertos < letterQtd and vidas>0:
 
         for indexLetter, currentLetter in enumerate(palavra):
             if letter == currentLetter:
+                for i in range(50):
+                    print("=" * 20)
                 print(f"Letra '{letter}' encontrada na posição {indexLetter +1}")
                 #colocando a letra encontrada na posição do _ do vetor da forca
                 wordGuess[indexLetter] = letter
@@ -57,28 +111,36 @@ while acertos < letterQtd and vidas>0:
                 notValid+=1
                 #a cada giro no for, adiciono +1 pra cada slot que a letra não estiver (continua abaixo)
 
-
         if notValid == letterQtd:
+            for i in range(50):
+                print("=" * 20)
             vidas-=1
+            estagioForca+=1
             print("[Opção incorreta]")
             #se o valor de notValid for igual a quantidade de letra da palavra, vou saber que n tem a letra na palavra
 
     else:
         #se a letra já estiver nas letras enviadas, não farei nada e indicarei que a letra ja´foi enviada
+        for i in range(50):
+            print("=" * 20)
         print(f"--------------------\nLetra já enviada\n--------------------")
 
 
     print(f"Vidas restantes: {vidas}")
 
 #final do while (acertou todas letras ou perdeu todas vidas)
-
 if vidas <= 0:
-    print(f"Você perdeu! a palavra era: {palavra}")
+    print(forcaDraw[estagioForca])
+    print(f"Você perdeu! a palavra era: {palavra.upper()}")
 else:
+    print(forcaDraw[estagioForca])
     print(f"Parabéns, você acertou a palavra: {palavra} com {vidas} vidas")
+
 
 dp = displayWord().upper()
 print(dp)
+print(f"Letras enviadas: {letterSend}")
 #print(ActiveWord)
 
-print(f"\n\nPressione qualquer tecla para fechar: ")
+print(f"\nPressione qualquer tecla para fechar: ")
+input()
