9de338f35c1fc4930c8c94ae1a7b1ceb074c9ecb
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-05-05 00:27:34 -0300

    Função para limpar console, deixar mais limpo visualmente

--- Desafio.py
+++ Desafio.py
@@ -1,3 +1,8 @@
+import os
+
+def clearConsole():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 forcaDraw = [
     """
   +---+
@@ -58,7 +63,6 @@ forcaDraw = [
 ]
 
 estagioForca=0
-
 palavra = input("Jogador desafiante, digite a palavra para o jogador 2: ").lower() or "Carro".lower()
 if palavra=="carro":
     categoria="Veiculo"
@@ -71,17 +75,14 @@ acertos=0
 vidas=6
 letterSend =[]
 
-for i in range(50):
-    print("="*20)
-    print("Não suba, não trapaceie")
-
-print(f"\nBem vindo ao jogo da forca!!! \n"
+clearConsole()
+print("==========================\n")
+print(f"Bem vindo ao jogo da forca!!! \n"
       f"Dica: {categoria}\n")
 
 wordGuess = ['_']*letterQtd
 
 def displayWord():
-
     return " , ".join(wordGuess[:letterQtd])
 
 while acertos < letterQtd and vidas>0:
@@ -90,52 +91,47 @@ while acertos < letterQtd and vidas>0:
     print(forcaDraw[estagioForca])
     dp = displayWord().upper()
     letter = str(input(f"{dp}\n\nDigite uma letra: ").lower())
+    clearConsole()
 
     if letter not in letterSend:
-        #se a letra enviada não estiver no vetor das letras enviadas, irei colocar ela dentro e darei sequencia no código
-
         letterSend.append(letter)
-
+        # se a letra enviada não estiver no vetor das letras enviadas, irei colocar ela dentro e darei sequencia no código
         print("==========================\n")
 
         for indexLetter, currentLetter in enumerate(palavra):
             if letter == currentLetter:
-                for i in range(50):
-                    print("=" * 20)
                 print(f"Letra '{letter}' encontrada na posição {indexLetter +1}")
                 #colocando a letra encontrada na posição do _ do vetor da forca
                 wordGuess[indexLetter] = letter
                 acertos+=1
-
             else:
                 notValid+=1
                 #a cada giro no for, adiciono +1 pra cada slot que a letra não estiver (continua abaixo)
 
         if notValid == letterQtd:
-            for i in range(50):
-                print("=" * 20)
+            clearConsole()
             vidas-=1
             estagioForca+=1
+            print("==========================\n")
             print("[Opção incorreta]")
             #se o valor de notValid for igual a quantidade de letra da palavra, vou saber que n tem a letra na palavra
-
     else:
         #se a letra já estiver nas letras enviadas, não farei nada e indicarei que a letra ja´foi enviada
-        for i in range(50):
-            print("=" * 20)
+        clearConsole()
+        print("==========================\n")
         print(f"--------------------\nLetra já enviada\n--------------------")
-
 
     print(f"Vidas restantes: {vidas}")
 
 #final do while (acertou todas letras ou perdeu todas vidas)
+clearConsole()
+print("==========================\n")
 if vidas <= 0:
     print(forcaDraw[estagioForca])
     print(f"Você perdeu! a palavra era: {palavra.upper()}")
 else:
     print(forcaDraw[estagioForca])
     print(f"Parabéns, você acertou a palavra: {palavra} com {vidas} vidas")
-
 
 dp = displayWord().upper()
 print(dp)
